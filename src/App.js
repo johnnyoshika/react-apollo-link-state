f38ba55eb0c847c1ba4fd5d68b080d0df8f3d13f
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-13 21:06:45 -0800

    List repositories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
-const App = () => (
-  <div>
-  </div>
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      repositories(first: 20) {
+        edges {
+          node {
+            id
+            name
+            url
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`;
+
+const App = () => {
+  const { data, loading, error } = useQuery(GET_REPOSITORIES_OF_ORGANIZATION);
+
+  if (loading && !data) return <div>Loading ...</div>;
+  
+  const { organization } = data;
+  if (!organization) return <div>Loading ...</div>;
+
+  return (
+    <div>
+      <RepositoryList repositories={organization.repositories} />
+    </div>
+  );
+};
+
+const RepositoryList = ({ repositories }) => (
+  <ul>
+    {repositories.edges.map(({ node }) => (
+        <li key={node.id}>
+          <a href={node.url}>{node.name}</a>
+        </li>
+     ))}
+  </ul>
 );
 
 export default App;
