48d092e04b3ed71af8cec1ff95a53533c4710fb1
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-13 21:14:01 -0800

    Add star repository mutation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 
 const GET_REPOSITORIES_OF_ORGANIZATION = gql`
   {
@@ -19,11 +19,23 @@ const GET_REPOSITORIES_OF_ORGANIZATION = gql`
   }
 `;
 
+const STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+
 const App = () => {
-  const { data, loading, error } = useQuery(GET_REPOSITORIES_OF_ORGANIZATION);
+  const { data, loading } = useQuery(GET_REPOSITORIES_OF_ORGANIZATION);
 
   if (loading && !data) return <div>Loading ...</div>;
-  
+
   const { organization } = data;
   if (!organization) return <div>Loading ...</div>;
 
@@ -37,11 +49,26 @@ const App = () => {
 const RepositoryList = ({ repositories }) => (
   <ul>
     {repositories.edges.map(({ node }) => (
-        <li key={node.id}>
-          <a href={node.url}>{node.name}</a>
-        </li>
-     ))}
+      <li key={node.id}>
+        <a href={node.url}>{node.name}</a>{' '}
+        {!node.viewerHasStarred && <Star id={node.id} />}
+      </li>
+    ))}
   </ul>
 );
+
+const Star = ({ id }) => {
+  const [addStar] = useMutation(STAR_REPOSITORY, {
+    variables: {
+      id
+    }
+  });
+
+  return (
+    <button type="button" onClick={addStar}>
+      Star
+    </button>
+  );
+};
 
 export default App;
